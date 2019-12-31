6df25f379101bc2119239f6bedc7659e1f143a9c
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:23:30 -0600

    Rewriting as Laminas Project package

--- test/TestAsset/MockCaptcha.php
+++ test/TestAsset/MockCaptcha.php
@@ -1,20 +1,18 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_Captcha
+ * @see       https://github.com/laminas/laminas-captcha for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-captcha/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-captcha/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Captcha\TestAsset;
+namespace LaminasTest\Captcha\TestAsset;
 
-use Zend\Captcha\AdapterInterface;
+use Laminas\Captcha\AdapterInterface;
 
 /**
- * @category   Zend
- * @package    Zend_Captcha
+ * @category   Laminas
+ * @package    Laminas_Captcha
  * @subpackage UnitTest
  */
 class MockCaptcha implements AdapterInterface
