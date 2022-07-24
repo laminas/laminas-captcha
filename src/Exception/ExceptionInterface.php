15420bfc4700fc1d3bcf8a3058301baf540bad67
George Steel <george@net-glue.co.uk>
2022-07-24 08:10:15 +0100

    Exception should extend from throwable
    Signed-off-by: George Steel <george@net-glue.co.uk>

--- src/Exception/ExceptionInterface.php
+++ src/Exception/ExceptionInterface.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace Laminas\Captcha\Exception;
 
+use Throwable;
+
 /**
  * Exception for Laminas\Form component.
  */
-interface ExceptionInterface
+interface ExceptionInterface extends Throwable
 {
 }
