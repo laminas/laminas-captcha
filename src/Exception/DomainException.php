9c0601ef1ca57ffc2ccefe7be1a893f44efd9433
Bhavin Parmar <cod62627@adobe.com>
2025-10-28 13:14:45 +0530

    PHP8.5 Support Added
    Signed-off-by: Bhavin Parmar <cod62627@adobe.com>

--- src/Exception/DomainException.php
+++ src/Exception/DomainException.php
@@ -1,4 +1,4 @@
-final <?php
+<?php
 
 declare(strict_types=1);
 
--- src/Exception/ImageNotLoadableException.php
+++ src/Exception/ImageNotLoadableException.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Laminafinal s\Captcha\Exception;
+namespace Laminafinals\Captcha\Exception;
 
 /**
  * Exception for Laminas\Form component.
